762b9a26f4ab001844dea8014df98410e66b9a20
Salsa <146612164+SalsaTriana@users.noreply.github.com>
2025-07-22 13:00:01 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Galeri otomatis
-let index = 0;
-const slides = document.querySelectorAll('.gallery-slider img');
-setInterval(() => {
-  slides.forEach((img, i) => img.style.display = 'none');
-  index = (index + 1) % slides.length;
-  slides[index].style.display = 'block';
-}, 3000);
